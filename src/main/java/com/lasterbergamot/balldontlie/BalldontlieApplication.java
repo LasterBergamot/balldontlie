bc71a495fee597f0d3347acef82f1dbc9d076e05
LasterBergamot <torokandras1996@gmail.com>
2020-10-01 15:16:36 +0200

    Initial commit.

---
+++ src/main/java/com/lasterbergamot/balldontlie/BalldontlieApplication.java
@@ -0,0 +1,13 @@
+package com.lasterbergamot.balldontlie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalldontlieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalldontlieApplication.class, args);
+	}
+
+}
