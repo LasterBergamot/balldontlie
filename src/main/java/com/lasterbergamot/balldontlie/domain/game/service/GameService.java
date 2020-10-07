864d181d1f0a29b4f6e5a1afab10c85bafbe895f
LasterBergamot <torokandras1996@gmail.com>
2020-10-07 11:10:11 +0200

    Added game related queries.

--- src/main/java/com/lasterbergamot/balldontlie/domain/game/service/GameService.java
+++ src/main/java/com/lasterbergamot/balldontlie/domain/game/service/GameService.java
@@ -2,9 +2,12 @@ package com.lasterbergamot.balldontlie.domain.game.service;
 
 import com.lasterbergamot.balldontlie.database.model.game.Game;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GameService {
     void getAllGamesFromBalldontlieAPI();
-    Optional<Game> getGame(Integer id);
+
+    List<Game> getGames(final int count);
+    Optional<Game> getGame(final int id);
 }
---
+++ src/main/java/com/lasterbergamot/balldontlie/graphql/game/query/GameQuery.java
@@ -0,0 +1,25 @@
+package com.lasterbergamot.balldontlie.graphql.game.query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.lasterbergamot.balldontlie.database.model.game.Game;
+import com.lasterbergamot.balldontlie.domain.game.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class GameQuery implements GraphQLQueryResolver {
+
+    private final GameService gameService;
+
+    public List<Game> getGames(final int count) {
+        return gameService.getGames(count);
+    }
+
+    public Optional<Game> getGame(final int id) {
+        return gameService.getGame(id);
+    }
+}
