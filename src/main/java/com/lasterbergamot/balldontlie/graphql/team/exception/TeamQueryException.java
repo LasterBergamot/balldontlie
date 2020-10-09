5aca37ad827aa546a3993bae1aab238e2e50adc8
LasterBergamot <torokandras1996@gmail.com>
2020-10-09 10:03:15 +0200

    Created parent GraphQLException class.
    Also created GameMutationException class which derives from GraphQLException.

--- src/main/java/com/lasterbergamot/balldontlie/graphql/team/exception/TeamQueryException.java
+++ src/main/java/com/lasterbergamot/balldontlie/graphql/team/exception/TeamQueryException.java
@@ -1,31 +1,9 @@
 package com.lasterbergamot.balldontlie.graphql.team.exception;
 
-import graphql.ErrorType;
-import graphql.GraphQLError;
-import graphql.language.SourceLocation;
+import com.lasterbergamot.balldontlie.graphql.exception.GraphQLException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-public class TeamQueryException extends RuntimeException implements GraphQLError {
-
+public class TeamQueryException extends GraphQLException {
     public TeamQueryException(String errorMessage) {
         super(errorMessage);
-    }
-
-    @Override
-    public Map<String, Object> getExtensions() {
-        return Map.of("errorMessage", getMessage());
-    }
-
-    @Override
-    public List<SourceLocation> getLocations() {
-        return new ArrayList<>();
-    }
-
-    @Override
-    public ErrorType getErrorType() {
-        return ErrorType.ValidationError;
     }
 }
